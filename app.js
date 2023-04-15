72ae60541f65b9948d9be63460a9b46eea1808cb
Fahd <64308102+fahd23@users.noreply.github.com>
2023-04-15 11:15:06 +0530

    Checkpoint 4

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-const username = "Fahd";
-const password = "123";
-
 document.getElementById("studentName").innerHTML =
   localStorage.getItem("username");
 
 function login() {
-  const user = document.getElementById("username").value;
-  const pass = document.getElementById("password").value;
-  if (user == username && pass == password) {
-    localStorage.setItem("username", user);
-    window.location.href = "welcome.html";
-  } else {
-    document.getElementById("loginError").style.display = "block";
-  }
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  fetch("db.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const users = data.students;
+      const user = users.find(
+        (i) => i.username === username && i.password === password
+      );
+      let userData = data.students.find(
+        (student) => student.username === username
+      );
+      if (user) {
+        localStorage.setItem("username", user.username);
+        window.location.href = "welcome.html";
+      } else {
+        document.getElementById("loginError").style.display = "block";
+      }
+
+      // Setting Value
+
+      document.getElementById("name").textContent = userData.username;
+      document.getElementById("dob").textContent = userData.dateOfBirth;
+      document.getElementById("email").textContent = userData.email;
+    })
+    .catch((error) => {
+      console.error("Error fetching student data:", error);
+    });
 }
 
 function hideLoginError() {
